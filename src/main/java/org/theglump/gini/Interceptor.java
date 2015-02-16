a55d04c0f3a834b78b24b398e79c4a852d954178
unknown <sebastien.rozange@MW773VKMUETGY6.dir.svc.accenture.com>
2015-02-16 15:22:12 +0100

    reduce object visibility

--- src/main/java/org/theglump/gini/Interceptor.java
+++ src/main/java/org/theglump/gini/Interceptor.java
@@ -2,11 +2,11 @@ package org.theglump.gini;
 
 import java.lang.reflect.Method;
 
-public class Interceptor {
-	public Object advice;
-	public Method method;
+class Interceptor {
+	Object advice;
+	Method method;
 
-	public Interceptor(Object advice, Method method) {
+	Interceptor(Object advice, Method method) {
 		this.advice = advice;
 		this.method = method;
 	}
